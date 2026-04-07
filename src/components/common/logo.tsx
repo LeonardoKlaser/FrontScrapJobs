f607f98158785e17ed0abbaef48658919a24e959
Leonardo Klaser <leonardo.klaser@sap.com>
2026-04-06 21:24:25 -0300

    fix favicon

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils'
+import faviconSrc from '@/assets/favicon.jpeg'
 
 interface LogoProps {
   size?: number
@@ -24,26 +25,13 @@ export function Logo({ size = 32, showText = false, className, textClassName }: 
 }
 
 export function LogoMark({ size = 32, className }: { size?: number; className?: string }) {
-  const r = size * 0.25
-
   return (
-    <svg
+    <img
+      src={faviconSrc}
+      alt="ScrapJobs"
       width={size}
       height={size}
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn('shrink-0', className)}
-    >
-      <defs>
-        <linearGradient id="sj-bg" x1="0" y1="0" x2="48" y2="48" gradientUnits="userSpaceOnUse">
-          <stop offset="0%" stopColor="#34d399" />
-          <stop offset="100%" stopColor="#059669" />
-        </linearGradient>
-      </defs>
-      <rect width="48" height="48" rx={r} fill="url(#sj-bg)" />
-      {/* Lightning bolt / S shape */}
-      <path d="M28 6L14 26h7l-4 16 14-18h-7l4-18z" fill="white" fillOpacity="0.95" />
-    </svg>
+      className={cn('shrink-0 rounded', className)}
+    />
   )
 }
